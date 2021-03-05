a7ee6f71f78c2910b1b32b86d59bfc10a650cfa1
EdwarTriana <edwarsteventriana@gmail.com>
2021-03-05 17:44:19 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function calculateResult(){
+    const  result = document.getElementById("result").innerHTML
+    return result
+    let infinity
+
+    
+}
+function resultado(value){
+    document.getElementById("result").innerHTML=value
+    
+}
+function place(key){
+    let result = calculateResult()
+    if (result != "0" || isNaN(key)){
+        console.log(isNaN(key)) 
+        resultado(result + key)
+    } else {
+        resultado(key)
+    }
+ 
+    
+}
+function calcular(){
+    let result = eval(calculateResult())
+        resultado(result)
+        if(place === "0"){
+            result = (place / document.getElementById("numero0").innerHTML )
+          }else if(result === Infinity){
+            alert("No puedes dividir por cero")
+          }
+      }
+function borrar(key){
+    resultado(0)
+}
+
